e9317944b42f27fff73406f039badc403adf5852
Solemarc <larco9mason@gmail.com>
2025-01-29 23:26:57 +1000

    impliment generic struct

--- src/reduce.rs
+++ src/reduce.rs
@@ -1,24 +1,37 @@
-#[allow(dead_code)]
-pub fn add<N: std::ops::Add<Output = N>>(input: Vec<N>) -> N {
-    input.into_iter().reduce(|a, b| a + b).unwrap()
+pub struct Reducer<N> {
+    data: Vec<N>,
 }
 
 #[allow(dead_code)]
-pub fn sub<N: std::ops::Sub<Output = N>>(input: Vec<N>) -> N {
-    input.into_iter().reduce(|a, b| a - b).unwrap()
-}
+impl<
+        N: std::ops::Add<Output = N>
+            + std::ops::Sub<Output = N>
+            + std::ops::Mul<Output = N>
+            + std::ops::Div<Output = N>
+            + std::clone::Clone,
+    > Reducer<N>
+{
+    pub fn new(data: Vec<N>) -> Self {
+        Reducer { data }
+    }
 
-#[allow(dead_code)]
-pub fn mul<N: std::ops::Mul<Output = N>>(input: Vec<N>) -> N {
-    input.into_iter().reduce(|a, b| a * b).unwrap()
-}
+    pub fn add(&self) -> N {
+        self.data.clone().into_iter().reduce(|a, b| a + b).unwrap()
+    }
 
-#[allow(dead_code)]
-pub fn div<N: std::ops::Div<Output = N>>(input: Vec<N>) -> N {
-    input.into_iter().reduce(|a, b| a / b).unwrap()
-}
+    pub fn sub(&self) -> N {
+        self.data.clone().into_iter().reduce(|a, b| a - b).unwrap()
+    }
 
-#[allow(dead_code)]
-pub fn other<N>(input: Vec<N>, func: fn(N, N) -> N) -> N {
-    input.into_iter().reduce(func).unwrap()
+    pub fn mul(&self) -> N {
+        self.data.clone().into_iter().reduce(|a, b| a * b).unwrap()
+    }
+
+    pub fn div(&self) -> N {
+        self.data.clone().into_iter().reduce(|a, b| a / b).unwrap()
+    }
+
+    pub fn other(&self, func: fn(N, N) -> N) -> N {
+        self.data.clone().into_iter().reduce(func).unwrap()
+    }
 }
--- src/test/reduce_test.rs
+++ src/test/reduce_test.rs
@@ -1,40 +1,35 @@
 #[cfg(test)]
 mod tests {
-    use crate::reduce::{add, div, mul, other, sub};
+    use crate::reduce::Reducer;
 
     #[test]
     fn test_add() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let result: i32 = add(input);
-        assert_eq!(55, result);
+        let red = Reducer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(55, red.add());
     }
 
     #[test]
     fn test_sub() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let result: i32 = sub(input);
-        assert_eq!(-53, result);
+        let red = Reducer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(-53, red.sub());
     }
 
     #[test]
     fn test_mul() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let result: i32 = mul(input);
-        assert_eq!(3628800, result);
+        let red = Reducer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(3628800, red.mul());
     }
 
     #[test]
     fn test_div() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let result: i32 = div(input);
-        assert_eq!(0, result);
+        let red = Reducer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(0, red.div());
     }
 
     #[test]
     fn test_other() {
-        let input: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let result: i32 = other(input, other_reduce);
-        assert_eq!(100, result);
+        let red = Reducer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert_eq!(100, red.other(other_reduce));
     }
 
     fn other_reduce(a: i32, b: i32) -> i32 {
