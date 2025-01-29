309302eabb53d08c1a630f9d68d8f2252980733a
Solemarc <larco9mason@gmail.com>
2025-01-29 23:26:42 +1000

    compare char instead of Option<char>

--- src/palindrome.rs
+++ src/palindrome.rs
@@ -1,8 +1,8 @@
 #[allow(dead_code)]
 pub fn palindrome(input: String) -> bool {
     for i in 0..input.len() {
-        let x = input.chars().nth(i);
-        let y = input.chars().nth(input.len() - (i + 1));
+        let x = input.chars().nth(i).unwrap_or_default();
+        let y = input.chars().nth(input.len() - (i + 1)).unwrap_or_default();
         if x != y {
             return false;
         }
