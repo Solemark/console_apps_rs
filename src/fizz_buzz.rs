07fe983700f1205d44378a8ce46498a03b60ee12
Solemarc <larco9mason@gmail.com>
2025-01-30 16:24:46 +1000

    simplify code

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -2,18 +2,12 @@
 pub fn fizz_buzz(fizz: i32, buzz: i32, max: i32) -> String {
     let mut output = String::new();
     for i in 1..=max {
-        output += &if_then_else(i % fizz == 0, "fizz".to_string());
-        output += &if_then_else(i % buzz == 0, "buzz".to_string());
-        output += &if_then_else(!output.ends_with('z'), i.to_string());
-        output += &if_then_else(i != max, ", ".to_string());
+        output += if i % fizz == 0 { "fizz" } else { "" };
+        output += if i % buzz == 0 { "buzz" } else { "" };
+        if !output.ends_with('z') {
+            output += &i.to_string();
+        }
+        output += if i != max { ", " } else { "" };
     }
     output
-}
-
-fn if_then_else(condition: bool, success: String) -> String {
-    if condition {
-        success
-    } else {
-        String::new()
-    }
 }
